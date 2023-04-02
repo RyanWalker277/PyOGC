8be179310f7782e609cbc768c16d5d41f7291ad2
Ryanwalker277 <andianurag277@gmail.com>
2023-04-02 23:54:54 +0530

    Removed duplicate import

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ here = os.path.abspath(os.path.dirname(__file__))
 with codecs.open(os.path.join(here, "README.md"), encoding="utf-8") as fh:
     long_description = "\n" + fh.read()
 
-VERSION = '0.0.2'
+VERSION = '0.0.3'
 DESCRIPTION = 'Unified OGC Api Python Client'
 LONG_DESCRIPTION = 'Python package that provides a simple and consistent interface for working with OGC (Open Geospatial Consortium) services'
 
